e1eb9ed99d2c0a567da8c9582bcdfff658e3d32b
jz_2003 <zhaojianxing2003@gmail.com>
2026-03-15 23:37:51 -0700

    github deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
+
 export default defineConfig({
+  base: isGitHubActions && repoName ? `/${repoName}/` : "/",
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used - do not remove them
